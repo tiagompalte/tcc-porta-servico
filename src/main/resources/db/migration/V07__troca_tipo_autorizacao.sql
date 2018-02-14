21c2c6352e2f699cb07b2b4653f429e36d81d634
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-02-14 12:59:07 -0200

    Correção de sintaxe no where do sql

--- src/main/resources/db/migration/V07__troca_tipo_autorizacao.sql
+++ src/main/resources/db/migration/V07__troca_tipo_autorizacao.sql
@@ -1,4 +1,4 @@
 
 ALTER TABLE autorizacao ADD COLUMN dia_mes INT(2);
 
-UPDATE autorizacao SET tipo_autorizacao = 'SEMANAL' where tipo_autorizacao == 'PROGRAMADO';
+UPDATE autorizacao SET tipo_autorizacao = 'SEMANAL' where tipo_autorizacao = 'PROGRAMADO';
